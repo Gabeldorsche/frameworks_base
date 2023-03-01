b9904c474a9a711bb836ca758c88d887125b5128
Luofan Chen <clfbbn@gmail.com>
2023-03-01 19:12:53 +0800

    base: Implement Lineage Health Service
    Change-Id: I772ccf6d323c24d681aa8468bf4318c7b73bd3f5
    
    ChargingControl: Add static isChargingControlSupported(ctx) method
    
    This will let us use it without bothering to handle RuntimeException
    from HealthInterface contructor.
    
    Change-Id: I768c250d1e527b6fd8e49a40452b98404062cc18
    
    ChargingControl: Set a small margin for devices with bypass support
    
    Change-Id: I1eddaa85f9dd47283af1fcd43d6fc207bcfd5546
    
    ChargingControl: Reset internal states on settings change
    
    Change-Id: Ib49d6b5b9190a71f5447125ed1aa70da894e6c18
    
    ChargingControl: Add cancel toggle for limit done notification
    
    Change-Id: I4f969ae87e1f9aa264113f044ee850def33ad20f
    
    ChargingControl: Handle cancel toggle correctly if bypass is not supported
    
    Change-Id: Ib7e208bba554b76a067d70280785290d5a0ba08d
    
    ChargingControl: No need to log a crash when not supported
    
    Instead of relying on catching the exception later on, check if
    ChargingControl is actually supported before checking if a specific
    mode is
    
    Change-Id: Icaf8d333842c9847bea00bedb4b302b7acd49bb3
    
    ChargingControl: Use ServiceManager.waitForDeclaredService()
    
    It might take a while until health HAL is up, thus let's just wait for
    it.
    
    Change-Id: Ie929a25a2a774aedb4559662e9ad0a47f1b154e6
    
    ChargingControl: Don't set deadline if charging control is not enabled
    
    Change-Id: I5e7570184294e8391e4908e449c1bffb07d9410d
    
    ChargingControl: Return early if lineage health HAL is not found
    
    Change-Id: I2616224167a4fe5e59bdf01c5e124744bb8b2d51
    
    ChargingControl: Drop default values for initially zero variables
    
    Java generally sets the default values for fields to zero or null.
    
    Change-Id: I7a993dfc70476ed5beb649cae42de2b125b897b9
    
    ChargingControl: Retry setting deadline if it fails
    
    Do not save the target time unless setting deadline succeeds,
    effectively allowing it to be retried later.
    
    Change-Id: I572b935b088170d56623a33c5efd2292b1a67126
    
    ChargingControl: Additional log lines for deadline
    
    Change-Id: I9ceb3b0df0095cb12408a6ae47be8fd8af6678a8
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -293,6 +293,7 @@ import com.android.server.webkit.WebViewUpdateService;
 import com.android.server.wm.ActivityTaskManagerService;
 import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
+import com.android.server.lineage.health.HealthInterfaceService;
 
 import dalvik.system.VMRuntime;
 
@@ -2676,6 +2677,10 @@ public final class SystemServer implements Dumpable {
             // PixysDeviceConfigService
             t.traceBegin("StartPixysDeviceConfigService");
             mSystemServiceManager.startService(PixysDeviceConfigService.class);
+            t.traceEnd();
+
+            t.traceBegin("StartHealthService");
+            mSystemServiceManager.startService(HealthInterfaceService.class);
             t.traceEnd();
 
             if (!com.android.server.flags.Flags.optionalBackgroundInstallControl()
