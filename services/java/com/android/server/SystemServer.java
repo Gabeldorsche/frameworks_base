bddf24099cee7b443f9600aec67580d4a6b5f18e
jhenrique09 <jhsv09@gmail.com>
2023-08-10 14:22:06 +0000

    [SQUASH] Implement DeviceConfigUtils and service
    - Allow overriding device configs while using GMS as device configurator package
    
    Change-Id: Ie7327610feca7a390915511088dfef831c946853
    
    DeviceConfigUtils: Make debug optional
    
    Change-Id: I65ffac8fd0a9971297b8d721d860face571a2d44
    (cherry picked from commit 9f54af5392d7ca70e1e495fc97560538459844e7)
    
    DeviceConfig: Change setProperties behavior
    
    Change-Id: I7cf1574f423c7362e6a7c9d8a002e4b20e5e7f87
    (cherry picked from commit f94a2c97e7bf53ee6681183f2394878369e99470)
    
    * Adapt to Android 14
    
    Change-Id: I6ece899c13f12ee299ba24b02f2b4ab96f3d92fb
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -210,6 +210,7 @@ import com.android.server.pdb.PersistentDataBlockService;
 import com.android.server.people.PeopleService;
 import com.android.server.permission.access.AccessCheckingService;
 import com.android.server.pinner.PinnerService;
+import com.android.server.pixys.PixysDeviceConfigService;
 import com.android.server.pm.ApexManager;
 import com.android.server.pm.ApexSystemServiceInfo;
 import com.android.server.pm.BackgroundInstallControlService;
@@ -2745,6 +2746,11 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartMediaMetricsManager");
             mSystemServiceManager.startService(MediaMetricsManagerService.class);
+            t.traceEnd();
+
+            // PixysDeviceConfigService
+            t.traceBegin("StartPixysDeviceConfigService");
+            mSystemServiceManager.startService(PixysDeviceConfigService.class);
             t.traceEnd();
 
             if (!com.android.server.flags.Flags.optionalBackgroundInstallControl()
